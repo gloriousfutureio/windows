4afe840478a6a4dbdf603a0a82ee6ad84e2f7cf3
Coleman McFarland <coleman@de.limited>
2021-01-30 17:45:01 -0500

    messing around

--- bin/anycommand.tcl
+++ bin/anycommand.tcl
@@ -13,9 +13,20 @@ package require example 1.0
 # TODO: make sure we don't pass empty commands. Empty args are ok.
 
 proc view {} {
+    menu .mbar
+
+    . config -menu .mbar
+    .mbar add cascade -label "System" -underline 0 \
+        -menu [menu .mbar.system -tearoff 0]
+
     frame .logview
+    # set up menu
+    #.mbar add checkbutton -label "OK" -command { puts "OK" }
+
+
+
     pack .logview -fill x
-    text .logview.main -relief flat -bd 2 -yscrollcommand ".logview.scroll set"
+    text .logview.main -relief flat -bd 2 -bg black -yscrollcommand ".logview.scroll set"
     scrollbar .logview.scroll -command ".logview.main yview"
     pack .logview.main -side left -fill y
     pack .logview.scroll -side right -fill y
@@ -27,6 +38,9 @@ proc view {} {
     pack .run.btn -side right -padx 10
     pack .run.cmd -side left
     pack .run.arg -side left -padx 5
+    frame .modes  -borderwidth 5
+    button .modes.ok -text "OK" -command { puts "button ok" }
+    pack .modes.ok -side left
 }
 
 proc execute {} {
@@ -37,3 +51,10 @@ proc execute {} {
     }
 }
 view
+#proc print_hierarchy {w {depth 0}} {
+#    puts "[string repeat "  " $depth][winfo class $w] w=[winfo width $w] h=[winfo height $w] x=[winfo x $w] y=[winfo y $w]"
+#    foreach i [winfo children $w] {
+#        print_hierarchy $i [expr {$depth+1}]
+#    }
+#}
+#print_hierarchy .
